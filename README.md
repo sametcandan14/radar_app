bc757244efa7bc5dddd55134a1296f0d0149ee88
Samet Candan <c._samet@hotmail.com>
2023-10-21 17:07:39 +0300

    packages installed

--- README.md
+++ README.md
@@ -14,3 +14,7 @@
 - Liste görünümü "ListView.jsx" bileşeni oluştur
 - Harita görünümü "MapView.jsx" bileşeni oluştur
 - App.js içerisinde bu iki görünüm arasında geçiş sağla
+
+"MapView.jsx" de harita(react leaflet) kurulumunu yap.
+
+-Toolkit ile beraber redux kurulumunu yap.
--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -1,5 +1,21 @@
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+
 const MapView = () => {
-  return <div>MapView</div>;
+  return (
+    <div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default MapView;
